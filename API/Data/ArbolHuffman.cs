997c279aeb32a79097631d67b161cfca3369f656
MarlonRoches <roches856@gmail.com>
2020-02-27 20:24:57 -0600

    Reinicio

--- API/Data/ArbolHuffman.cs
+++ API/Data/ArbolHuffman.cs
@@ -33,62 +33,9 @@ namespace API.Data
         #endregion
         public void Comprimir(string _root)   
         {
-            Root = _root;
-            ObtenerFrecuencias();
-            ArmarArbol();
-
-        }
-
-        public void ArmarArbol()
-        {
-            Frecuencias = Diccionario;
-            var temp = Diccionario.ToArray();
+            patth = _root;
             
-            var contador = 1    ;
-            while (Diccionario.Count != 1)
-            {
-                //juntar
-                var nuevo = new NodoHuffman($"C{contador}")
-                {
-
-                }
-
-                //eliminar
-                //Ordenar
-
-            }
         }
-        public void ObtenerFrecuencias()
-        {
-            var reader = new StreamReader(Root);
-            var texto = reader.ReadToEnd();
-            foreach (var item in texto)
-            {
-                if (Diccionario.ContainsKey(item.ToString()))
-                {
-                    // lo contiene
-                    Diccionario[item.ToString()].Frecuencia++;
-                }
-                else
-                {
-                    //nuevo
-                    Diccionario.Add(item.ToString(), new NodoHuffman(item.ToString()));
-                }
-            }
-            foreach (var item in Diccionario)
-            {
-                item.Value.Frecuencia = item.Value.Frecuencia / texto.Length;
-            }
-            var Sorting = Diccionario.ToList();
-            Sorting.Sort((x, y) => x.Value.Frecuencia.CompareTo(y.Value.Frecuencia));
-            Diccionario = new Dictionary<string, NodoHuffman>();
-            foreach (var item in Sorting)
-            {
-                Diccionario.Add(item.Key, item.Value);
-            }
-
-            //C:\Users\roche\Desktop\Tony\Lab1Compresion_\Compresion\BIBLIA COMPLETA.txt
-            reader.Close();
-        }
+        
     }
 }
